0c843e77efa2a92ea9a70ffd43f4037391e3f95d
BigWaffleMonster <rachkov.daniil2001@gmail.com>
2024-02-06 13:29:45 +0300

    q

--- init.lua
+++ init.lua
@@ -1,13 +1,78 @@
 return {
-  colorscheme = "gruvbox",
+  colorscheme = "gruvbox-material",
+  -- colorscheme = "solarized-osaka",
 
   plugins = {
+    -- {
+    --   "morhetz/gruvbox",
+    --   name = "gruvbox",
+    --   -- config = function()
+    --   --   require("gruvbox").setup {}
+    --   -- end,
+    -- },
     {
-      "morhetz/gruvbox",
-      name = "gruvbox",
-      -- config = function()
-      --   require("gruvbox").setup {}
-      -- end,
+      "craftzdog/solarized-osaka.nvim",
+      name = "solarized-osaka",
+      lazy = false,
+      priority = 1000,
+      opts = {},
+      config = function()
+        require("solarized-osaka").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          transparent = false, -- Enable this to disable setting the background color
+          terminal_colors = false,
+        }
+      end,
+    },
+    {
+      "xiyaowong/transparent.nvim",
+      name = "transparent",
+      lazy = false,
+    },
+    {
+      "folke/zen-mode.nvim",
+      lazy = false,
+      opts = {},
+    },
+    {
+      "sainnhe/gruvbox-material",
+      name = "gruvbox-material",
+      config = function()
+        vim.g.gruvbox_material_foreground = "mix"
+        vim.g.gruvbox_material_background = "hard"
+        vim.cmd.colorscheme "gruvbox-material"
+      end,
+    },
+    {
+      "rebelot/kanagawa.nvim",
+      name = "kanagawa",
+      config = function()
+        require("kanagawa").setup {
+          compile = false, -- enable compiling the colorscheme
+          undercurl = true, -- enable undercurls
+          commentStyle = { italic = true },
+          functionStyle = {},
+          keywordStyle = { italic = true },
+          statementStyle = { bold = true },
+          typeStyle = {},
+          transparent = false, -- do not set background color
+          dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+          terminalColors = true, -- define vim.g.terminal_color_{0,17}
+          colors = { -- add/modify theme and palette colors
+            palette = {},
+            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+          },
+          overrides = function(colors) -- add/modify highlights
+            return {}
+          end,
+          theme = "dragon", -- Load "wave" theme when 'background' option is not set
+          background = { -- map the value of 'background' option to a theme
+            dark = "dragon", -- try "dragon" !
+            light = "lotus",
+          },
+        }
+      end,
     },
     { "mattn/emmet-vim" },
     {
